9373f95d2d3d76dc07821bfa5001c46abf6d1641
Thiago <martinsmthiago@gmail.com>
2023-03-22 14:43:17 -0300

    feat:added main package tests

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,49 +14,61 @@ import (
 	"github.com/thiago-martinss/bookings/internal/render"
 )
 
-const portNumber = ":8080"
 
-var app config.AppConfig
-var session *scs.SessionManager
+	const portNumber = ":8080"
 
-// main is the main function
-func main() {
-	// what am I going to put in the session
-	gob.Register(models.Reservation{})
-	// change this to true when in production
-	app.InProduction = false
-
-	// set up the session
-	session = scs.New()
-	session.Lifetime = 24 * time.Hour
-	session.Cookie.Persist = true
-	session.Cookie.SameSite = http.SameSiteLaxMode
-	session.Cookie.Secure = app.InProduction
-
-	app.Session = session
-
-	tc, err := render.CreateTemplateCache()
-	if err != nil {
-		log.Fatal("cannot create template cache")
+	var app config.AppConfig
+	var session *scs.SessionManager
+	
+	// main is the main function
+	func main() {
+		err := run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+		fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	
+		srv := &http.Server{
+			Addr:    portNumber,
+			Handler: routes(&app),
+		}
+	
+		err = srv.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	app.TemplateCache = tc
-	app.UseCache = false
+	func run() error {
 
-	repo := handlers.NewRepo(&app)
-	handlers.NewHandlers(repo)
+		// what am I going to put in the session
+		gob.Register(models.Reservation{})
+		// change this to true when in production
+		app.InProduction = false
+	
+		// set up the session
+		session = scs.New()
+		session.Lifetime = 24 * time.Hour
+		session.Cookie.Persist = true
+		session.Cookie.SameSite = http.SameSiteLaxMode
+		session.Cookie.Secure = app.InProduction
+	
+		app.Session = session
+	
+		tc, err := render.CreateTemplateCache()
+		if err != nil {
+			log.Fatal("cannot create template cache")
+			return err
+		}
+	
+		app.TemplateCache = tc
+		app.UseCache = false
+	
+		repo := handlers.NewRepo(&app)
+		handlers.NewHandlers(repo)
+	
+		render.NewTemplates(&app)
 
-	render.NewTemplates(&app)
-
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
-
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: routes(&app),
-	}
-
-	err = srv.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+		return nil
+   }
