bb6a88209c7d7c474382c98edff8b20fed3aa388
Thiago <martinsmthiago@gmail.com>
2023-04-03 16:20:18 -0300

    feat: added insert Room Restrictions function

--- Internal/repository/dbrepo/postgres.go
+++ Internal/repository/dbrepo/postgres.go
@@ -11,15 +11,18 @@ func (m *postgresDBRepo) AllUsers() bool {
 	return true
 }
 
-func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
+// InsertReservations inserts a reservation into the database
+func (m *postgresDBRepo) InsertReservation(res models.Reservation) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
-	
+
+var newID int
+
 	stmt := `insert into reservations (first_name, last_name, email, phone, start_date,
 		end_date, room_id, created_at, updated_at)
-		values ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+		values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning id`
 
-		_, err := m.DB.ExecContext(ctx,stmt,
+		err := m.DB.QueryRowContext(ctx,stmt,
 			res.FirstName,
 			res.LastName,
 			res.Email,
@@ -30,10 +33,69 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 			time.Now(),
 			time.Now(),
 		
+		).Scan(&newID)
+		if err != nil {
+			return 0, err
+		}
+
+	return  newID, nil
+}
+
+
+// InsertRoomRestriction inserts a room restriction into the database
+func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error{
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+
+
+	stmt := `insert into room_restrictions (start_date, end_date, room_id,
+		 reservation_id, created_at, updated_at, restriction_id)
+		 values
+		 ($1, $2, $3, $4, $5, $6, $7)`
+
+		_, err := m.DB.ExecContext(ctx, stmt,
+			r.StartDate,
+			r.EndDate,
+			r.RoomID,
+			r.ReservationID,
+			time.Now(),
+			time.Now(),
+			r.RestrictionID,
 		)
+
 		if err != nil {
 			return err
 		}
 
-	return nil
+		return nil
+}
+
+// SearchAvailabityByDates returns true if availability exists for roomID, and false if no availability
+func(m *postgresDBRepo)  SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+var numRows int
+
+query := `
+	select 
+		count(id) 
+	from 
+		room_restrictions
+	where 
+		room_id = $1
+		$2 < end_date and $3 > start_date;`
+
+row  := m.DB.QueryRowContext(ctx, query, roomID, start, end)
+err := row.Scan(&numRows)
+if err != nil {
+	return false, err
+}
+
+if numRows == 0 {
+	return true, nil
+}
+return false, nil
+
 }
