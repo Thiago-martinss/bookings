6e267ad8046240d76e7ad730650aa5c3f67840fc
Thiago <martinsmthiago@gmail.com>
2023-04-06 17:24:25 -0300

    feat: add test database repository

---
+++ Internal/repository/dbrepo/test-repo.go
@@ -0,0 +1,42 @@
+package dbrepo
+
+import (
+	"time"
+
+	"github.com/thiago-martinss/bookings/internal/models"
+)
+
+func (m *testDBRepo) AllUsers() bool {
+	return true
+}
+
+// InsertReservations inserts a reservation into the database
+func (m *testDBRepo) InsertReservation(res models.Reservation) (int, error) {
+	
+
+	return 1, nil
+}
+
+// InsertRoomRestriction inserts a room restriction into the database
+func (m *testDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
+	return nil
+}
+
+// SearchAvailabityByDatesByRoomID returns true if availability exists for roomID, and false if no availability
+func (m *testDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error) {
+	return false, nil
+
+}
+
+// SearchAvailabityForAllRooms returns a slice of available rooms, if any, for given date range
+
+func (m *testDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) {
+	var rooms []models.Room
+	return rooms, nil
+}
+
+// GetRoomByID gets a room by id
+func (m *testDBRepo) GetRoomByID(id int) (models.Room, error) {
+	var room models.Room
+	return room, nil
+}
