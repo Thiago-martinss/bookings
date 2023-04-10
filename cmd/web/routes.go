71bc0e73186080a980403fd3337b348b121c45b4
Thiago <martinsmthiago@gmail.com>
2023-04-10 16:35:14 -0300

    feat: add PostShowLogin function

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -35,6 +35,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Post("/make-reservation", handlers.Repo.PostReservation)
 	mux.Get("/reservation-summary", handlers.Repo.ReservationSummary)
 	mux.Get("/user/login", handlers.Repo.ShowLogin)
+	mux.Post("/user/login", handlers.Repo.PostShowLogin)
+
 
 
 
