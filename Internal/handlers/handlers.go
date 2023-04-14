5c895c6bb60b1c955bddba83a5a3c695ea3cd92e
Thiago <martinsmthiago@gmail.com>
2023-04-14 15:59:25 -0300

    feat: add updated and delete reservations  function

--- Internal/handlers/handlers.go
+++ Internal/handlers/handlers.go
@@ -406,6 +406,7 @@ func (m *Repository) BookRoom(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// ShowLogin shows the login screen
 func (m *Repository) ShowLogin(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "login.page.html", &models.TemplateData{
 		Form: forms.New(nil),
@@ -462,11 +463,25 @@ func (m *Repository) AdminDashboard(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (m *Repository) AdminNewReservations(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, r, "admin-new-reservations.page.html", &models.TemplateData{})
 
+// AdminNewReservations shows all new reservations in admin tools
+func (m *Repository) AdminNewReservations(w http.ResponseWriter, r *http.Request) {
+	reservations, err := m.DB.AllNewReservations()
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+	
+	data := make(map[string]interface{})
+	data["reservations"] = reservations
+	
+	
+	render.Template(w, r, "admin-new-reservations.page.html", &models.TemplateData{
+		Data:data,
+	})
 }
 
+// AdminNewReservations shows all  reservations in admin tools
 func (m *Repository) AdminAllReservations(w http.ResponseWriter, r *http.Request) {
 	reservations, err := m.DB.AllReservations()
 	if err != nil {
@@ -477,13 +492,44 @@ func (m *Repository) AdminAllReservations(w http.ResponseWriter, r *http.Request
 	data := make(map[string]interface{})
 	data["reservations"] = reservations
 	
-	render.Template(w, r, "admin-all-reservations.page.html", &models.TemplateData {
+		render.Template(w, r, "admin-all-reservations.page.html", &models.TemplateData {
 		Data: data,
 	})
 
 }
 
+//AdminShowReservation shows the reservation in the admin tool
+func (m *Repository) AdminShowReservation(w http.ResponseWriter, r *http.Request) {
+		exploded := strings.Split(r.RequestURI, "/")
+		id, err := strconv.Atoi(exploded[4])
+		if err != nil {
+			helpers.ServerError(w, err)
+			return
+		}
+
+		src := exploded[3]
+
+		stringMap := make(map[string]string)
+		stringMap["src"] = src
+
+			res, err := m.DB.GetReservationByID(id)
+		if err != nil {
+			helpers.ServerError(w, err)
+			return
+		}
+
+		data := make(map[string]interface{})
+		data["reservation"] = res
+		render.Template(w, r, "admin-reservations-show.page.html", &models.TemplateData{
+			StringMap: stringMap,
+			Data: data,
+			Form: forms.New(nil),
+		})
+
+}
+
+// AdminReservationsCalendar displays the reservations calendar
 func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, r, "admin-reservations-calendar.html", &models.TemplateData{})
+		render.Template(w, r, "admin-reservations-calendar.html", &models.TemplateData{})
 
 }
--- Internal/repository/dbrepo/dbrepo.go
+++ Internal/repository/dbrepo/dbrepo.go
@@ -31,3 +31,4 @@ func NewTestingRepo(a *config.AppConfig) repository.DatabaseRepo {
 	
 	}
 }
+
