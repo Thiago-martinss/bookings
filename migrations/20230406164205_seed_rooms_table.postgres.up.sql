b5758767f2e4058149b272a8d5fd23c1828bd8da
Thiago <martinsmthiago@gmail.com>
2023-04-18 17:01:17 -0300

    refactor: split migrations

--- migrations/20230406164205_seed_rooms_table.postgres.up.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO "public"."rooms" ("id", "room_name", "created_at", "updated_at") VALUES
-(1, 'General''s Quarter', '2020-07-02 00:00:00', '2020-08-02 00:00:00'),
-(2, 'Major''s Suite', '2020-07-03 00:00:00', '2020-08-04 00:00:00');
-
--- migrations/20230406164953_seed_restrictions_table.postgres.up.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO "public"."restrictions" ("id", "restriction_name", "created_at", "updated_at") VALUES
-(1, 'Reservation', '2018-11-18 00:00:00', '2018-11-18 00:00:00'),
-(2, 'Owner Block', '2020-11-19 00:00:00', '2020-11-19 00:00:00');
